5e0116ff17615f6c237db921637ffced24f21f0e
RJ <ryanjayleyva@proton.me>
2025-02-18 08:39:34 -0500

    import global styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './app.tsx'
 import Header from './components/Header/Header.tsx'
+import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
