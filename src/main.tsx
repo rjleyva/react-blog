bbf6f82951e05cb39beaea3699c3ab21f9c18c5b
RJ <ryanjayleyva@proton.me>
2025-02-18 08:35:07 -0500

    import header component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './app.tsx'
+import Header from './components/Header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header home="ryanjayleyva" />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
