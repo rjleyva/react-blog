92cc9a15bfaa282f6b89d24dde81c08168962bd1
RJ <ryanjayleyva@proton.me>
2025-02-18 08:13:23 -0500

    import alias resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { fileURLToPath, URL } from 'url'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import Checker from 'vite-plugin-checker' // Correct import of vite-plugin-checker
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), Checker({ typescript: true })],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  },
+  server: {
+    port: 3000
+  }
 })
