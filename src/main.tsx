44f8dfc2ee6d384c73022bdd4f868fda6dc0644b
RJ <ryanjayleyva@proton.me>
2025-02-19 03:46:55 -0500

    import noise background component

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,20 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import App from './app.tsx'
 import Header from './components/Header/Header.tsx'
+import NoiseBackground from './components/NoiseBackground/NoiseBackground.tsx'
+import { ThemeProvider } from './contexts/ThemeContext.tsx'
 import './styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Header home="ryanjayleyva" />
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <ThemeProvider>
+        <NoiseBackground />
+        <Header home="ryanjayleyva" />
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 )
